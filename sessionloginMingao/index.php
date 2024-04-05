898767d69b258794cc6801f78942933d6d43be1d
lancelevi05 <156309396+lancelevi05@users.noreply.github.com>
2024-04-05 19:40:45 +0800

    Add files via upload

---
+++ sessionloginMingao/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if(isset($_SESSION['user_id'])) {
+    $username = $_SESSION['user_id'];
+    echo "Welcome, $username! You are logged in.<br>";
+    echo '<a href="logout.php">Logout</a>';
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
---
+++ sessionlogindatabaseMalazarte/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if(isset($_SESSION['user_id'])) {
+    $welcome = "Welcome, ".$_SESSION['username']."!<br>";
+    $logout= '<a href="logout.php">Logout</a>';
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card">
+        <?php echo $welcome; ?>
+        <?php echo $logout; ?>
+    </div>
+</body>
+</html>
+
+
